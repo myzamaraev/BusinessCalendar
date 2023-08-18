94d65793c65bf68512bbe7db358969aabfd6170c
m.zamaraev <myzamaraev@gmail.com>
2023-08-18 22:28:45 +0300

    additional checks and tests for CompactCalendarValidator

--- BusinessCalendar.Domain/Validators/CompactCalendarValidator.cs
+++ BusinessCalendar.Domain/Validators/CompactCalendarValidator.cs
@@ -1,5 +1,7 @@
 using System.Linq.Expressions;
 using BusinessCalendar.Domain.Dto;
+using BusinessCalendar.Domain.Extensions;
+using BusinessCalendar.Domain.Providers;
 using FluentValidation;
 using FluentValidation.Internal;
 using FluentValidation.Results;
@@ -8,28 +10,45 @@ namespace BusinessCalendar.Domain.Validators;
 
 public class CompactCalendarValidator : AbstractValidator<CompactCalendar>
 {
-    public CompactCalendarValidator(CalendarIdValidator calendarIdValidator)
+    public CompactCalendarValidator(IValidator<CalendarId> calendarIdValidator)
     {
         RuleFor(x => x.Id)
             .SetValidator(calendarIdValidator)
             .DependentRules(() =>
             {
-                AddDateValidationRules(x => x.Holidays);
-                AddDateValidationRules(x => x.ExtraWorkDays);
+                AddDateValidationRules(x => x.Holidays, DateMustBe.Workday);
+                AddDateValidationRules(x => x.ExtraWorkDays, DateMustBe.Weekend);
             });
     }
-    
-    private void AddDateValidationRules(Expression<Func<CompactCalendar, IEnumerable<DateOnly>>> expression)
+
+    private void AddDateValidationRules(Expression<Func<CompactCalendar, IEnumerable<DateOnly>>> expression, DateMustBe dateMustBe)
     {
         var expressionBody = (MemberExpression)expression.Body;
         var propName = expressionBody.Member.Name;
-
+        
         RuleForEach(expression)
             .Must((calendar, date) => date.Year == calendar.Id.Year)
-            .WithMessage($"Each date in {propName} must be part of the year");
+            .WithMessage((calendar, date) => $"{propName}: Date {date} has year different from {calendar.Id.Year}")
+            //Check every item in Holidays is workday
+            .Must(date => !date.IsWeekend())
+            .When(_ => dateMustBe == DateMustBe.Workday, ApplyConditionTo.CurrentValidator)
+            .WithMessage((_, date) => $"{propName}: Date {date} is {GetWeekdayDescription(date)} by default.")
+            //Check every item in ExtraWorkdays is weekend
+            .Must(date => date.IsWeekend())
+            .When(_ => dateMustBe == DateMustBe.Weekend, ApplyConditionTo.CurrentValidator)
+            .WithMessage((_, date) => $"{propName}: Date {date} is {GetWeekdayDescription(date)} by default.");
 
         RuleFor(expression)
             .Must(days => days.Distinct().Count() == days.Count())
             .WithMessage($"{propName} array has duplicate dates");
     }
+
+    private static string GetWeekdayDescription(DateOnly date) => date.IsWeekend() ? "weekend" : "workday";
+    
+    private enum DateMustBe : byte
+    {
+        Workday = 0,
+        Weekend = 1
+    }
 }
+
---
+++ BusinessCalendar.Tests/Domain/Validators/CompactCalendarValidatorTests.cs
@@ -0,0 +1,125 @@
+using BusinessCalendar.Domain.Dto;
+using BusinessCalendar.Domain.Validators;
+using FluentAssertions;
+using FluentValidation;
+using FluentValidation.Results;
+using Moq;
+
+namespace BusinessCalendar.Tests.Domain.Validators;
+
+[TestFixture]
+public class CompactCalendarValidatorTests
+{
+    private Mock<CalendarIdValidator> _calendarIdValidatorMock;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _calendarIdValidatorMock = new Mock<CalendarIdValidator>();
+    }
+
+    [Test]
+    public void Should_call_CalendarIdValidator()
+    {
+        var compactCalendar = new CompactCalendar(new CalendarId());
+
+        var validator = new CompactCalendarValidator(_calendarIdValidatorMock.Object);
+
+        validator.Validate(compactCalendar);
+
+        _calendarIdValidatorMock.Verify(x =>
+                x.Validate(It.Is<ValidationContext<CalendarId>>(context =>
+                    context.InstanceToValidate == compactCalendar.Id)),
+            Times.Once);
+    }
+
+    [Test]
+    public void Should_fail_when_duplicateDates()
+    {
+        var compactCalendar = new CompactCalendar(new CalendarId() { Year = Constants.CurrentYear })
+        {
+            Holidays =
+            {
+                DateOnly.FromDateTime(DateTime.Today),
+                DateOnly.FromDateTime(DateTime.Today)
+            },
+            ExtraWorkDays =
+            {
+                DateOnly.FromDateTime(DateTime.Today),
+                DateOnly.FromDateTime(DateTime.Today)
+            }
+        };
+
+        var validator = new CompactCalendarValidator(_calendarIdValidatorMock.Object);
+
+        var result = validator.Validate(compactCalendar);
+
+        result.Should().NotBeNull();
+        result.Errors.Should().Contain(failure => failure.ErrorMessage == "Holidays array has duplicate dates");
+        result.Errors.Should().Contain(failure => failure.ErrorMessage == "ExtraWorkDays array has duplicate dates");
+    }
+    
+    [Test]
+    public void Should_fail_when_date_out_of_year()
+    {
+        var compactCalendar = new CompactCalendar(new CalendarId() { Year = Constants.CurrentYear })
+        {
+            Holidays = { new DateOnly(Constants.CurrentYear+1,1,1) },
+            ExtraWorkDays = { new DateOnly(Constants.CurrentYear-1,12,31) }
+        };
+
+        var validator = new CompactCalendarValidator(_calendarIdValidatorMock.Object);
+
+        var result = validator.Validate(compactCalendar);
+
+        result.Should().NotBeNull();
+        result.Errors.Should().Contain(failure => failure.ErrorMessage == "Holidays: Date 01.01.2024 has year different from 2023");
+        result.Errors.Should().Contain(failure => failure.ErrorMessage == "ExtraWorkDays: Date 31.12.2022 has year different from 2023");
+    }
+    
+    [Test]
+    public void Should_fail_when_date_has_same_default_value()
+    {
+        var compactCalendar = new CompactCalendar(new CalendarId() { Year = Constants.CurrentYear })
+        {
+            Holidays = { new DateOnly(2023,1,1) },
+            ExtraWorkDays = { new DateOnly(2023,1,2) }
+        };
+
+        var validator = new CompactCalendarValidator(_calendarIdValidatorMock.Object);
+
+        var result = validator.Validate(compactCalendar);
+
+        result.Should().NotBeNull();
+        result.Errors.Should().Contain(failure => failure.ErrorMessage == "Holidays: Date 01.01.2023 is weekend by default.");
+        result.Errors.Should().Contain(failure => failure.ErrorMessage == "ExtraWorkDays: Date 02.01.2023 is workday by default.");
+    }
+    
+
+    [Test]
+    public void Should_return_when_CalendarIdValidator_failed()
+    {
+        var compactCalendar = new CompactCalendar(new CalendarId())
+        {
+            //duplicate dates should not cause additional errors in this scenario because this rule should depend on CalendarId validation result
+            Holidays =
+            {
+                DateOnly.FromDateTime(DateTime.Today),
+                DateOnly.FromDateTime(DateTime.Today)
+            }
+        };
+
+        var calendarIdValidator = new InlineValidator<CalendarId>()
+        {
+            v => v.RuleFor(x => x).Must(x => false).WithMessage("Failure")
+        };
+
+        var validator = new CompactCalendarValidator(calendarIdValidator);
+
+        var result = validator.Validate(compactCalendar);
+
+        result.Should().NotBeNull();
+        result.Errors.Should().HaveCount(1);
+        result.Errors.Should().Contain(failure => failure.ErrorMessage == "Failure");
+    }
+}
