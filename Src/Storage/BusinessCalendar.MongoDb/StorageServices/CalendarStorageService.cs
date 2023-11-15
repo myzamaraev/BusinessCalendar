0c210b759c8e121a5eec797fe7a7dbdf61922adc
m.zamaraev <myzamaraev@gmail.com>
2023-11-16 00:50:27 +0400

    add unique index by Id fields to Calendar collection in mongo

--- Src/Storage/BusinessCalendar.MongoDb/StorageServices/CalendarStorageService.cs
+++ Src/Storage/BusinessCalendar.MongoDb/StorageServices/CalendarStorageService.cs
@@ -14,6 +14,8 @@ namespace BusinessCalendar.MongoDb.StorageServices
         {
             var database = mongoClient.GetDatabase(mongoDbSettings.Value.DatabaseName);
             _calendarCollection = database.GetCollection<CompactCalendar>("Calendar");
+
+            EnsureCompositeIdIndex(_calendarCollection);
         }
 
         public async Task UpsertAsync(CompactCalendar compactCalendar, CancellationToken cancellationToken = default)
@@ -39,6 +41,26 @@ namespace BusinessCalendar.MongoDb.StorageServices
         public async Task DeleteManyAsync(CalendarType type, string key, CancellationToken cancellationToken = default)
         {
             await _calendarCollection.DeleteManyAsync(x => x.Id.Type == type && x.Id.Key == key, cancellationToken: cancellationToken);
+        }
+        
+        /// <summary>
+        /// creates a unique index by _id fields if not exists
+        /// PK uses hash of _id object, so unique constraint can be violated by changing the order of fields
+        /// this unique index prevents violations and covers search queries
+        /// </summary>
+        /// <param name="calendarCollection"></param>
+        private static void EnsureCompositeIdIndex(IMongoCollection<CompactCalendar> calendarCollection)
+        {
+            var indexKeys = Builders<CompactCalendar>.IndexKeys
+                .Ascending(x => x.Id.Type)
+                .Ascending(x => x.Id.Key)
+                .Ascending(x => x.Id.Year);
+            
+            var indexModel = new CreateIndexModel<CompactCalendar>(indexKeys, new CreateIndexOptions
+            {
+                Unique = true
+            });
+            calendarCollection.Indexes.CreateOne(indexModel);
         }
     }
 }
